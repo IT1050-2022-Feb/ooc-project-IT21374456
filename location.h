ef4f35c89250e60911a80b18756af9679d87c329
IT21374456 <100533796+IT21374456@users.noreply.github.com>
2022-05-20 23:26:26 +0530

    location.h completed

---
+++ location.h
@@ -0,0 +1,11 @@
+#include "reservation.h"
+
+class country{
+    private:
+        char countryName[30];
+    public:
+        countryName();       
+        countryName(const char pcountryName[]);       
+        void displayCountry();
+        ~countryName();
+};
