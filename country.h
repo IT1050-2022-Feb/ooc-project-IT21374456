10b7c780d8ece0494bddbf1699688230a1f3498e
IT21374456 <100533796+IT21374456@users.noreply.github.com>
2022-05-20 23:25:43 +0530

    country.h completed

---
+++ country.h
@@ -0,0 +1,11 @@
+#include "location.h"
+
+class country{
+    private:
+        char countryName[30];
+    public:
+        countryName();       
+        countryName(const char pcountryName[]);       
+        void displayCountry();
+        ~countryName();
+};
